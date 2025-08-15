820cbb7767aaed63c76fa4f48fc67c5e75fcd9a6
Lukas Neef <lukas.neef@tum.de>
2025-08-15 14:54:42 +0200

    fix ext linux boot

--- pci/pci-boot.c
+++ pci/pci-boot.c
@@ -1,9 +1,7 @@
-
 #include "cpu_func.h"
 #include "dm/device.h"
 #include "linux/errno.h"
 #include <pci-boot.h>
-
 int load_image(struct udevice* dev){
     u16 cmd;
     struct pci_boot_data* pbd = dev_get_priv(dev);
@@ -19,3 +17,7 @@ int load_image(struct udevice* dev){
     invalidate_dcache_range(pbd->data_addr, pbd->data_addr + 0x2000000);
     return 0; 
 }
+
+
+
+
